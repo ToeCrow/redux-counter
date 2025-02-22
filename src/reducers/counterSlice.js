238fdbeb65b93908e161984ae7749585b192fff7
ToeCrow <thomas.kronvold@chasacademy.se>
2025-02-22 18:37:57 +0100

    nr 1 done

--- src/reducers/counterSlice.js
+++ src/reducers/counterSlice.js
@@ -1,9 +1,14 @@
 import {createSlice} from '@reduxjs/toolkit'
 
+const savedCountFromStorage = localStorage.getItem("savedCount")
+  ? parseInt(localStorage.getItem("savedCount"), 10)
+  : 0;
+
 export const counterSlice = createSlice({
   name: 'counter',
   initialState: {
-    count: 0
+    count: 0,
+    savedCount: savedCountFromStorage 
   },
   reducers: {
     increment: (state) => {
@@ -14,10 +19,25 @@ export const counterSlice = createSlice({
     },
     incrementByAmount: (state, action) => {
       state.count += action.payload
+    },
+    double: (state) => {
+      state.count = state.count * 2
+    },
+    save: (state) => {
+      state.savedCount = state.count;
+      localStorage.setItem("savedCount", state.count);
+    },
+    reset: (state) => {
+      state.count = 0;
+      state.savedCount = 0;
+      localStorage.removeItem("savedCount");
+    },
+    addSaved: (state) => {
+      state.count += state.savedCount;
     }
   }
 })
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const { increment, decrement, incrementByAmount, double, save, reset, addSaved } = counterSlice.actions
 
 export default counterSlice.reducer
