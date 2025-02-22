02daf56dd50278399f6ab66818c75df812db1a83
ToeCrow <thomas.kronvold@chasacademy.se>
2025-02-22 12:40:23 +0100

    done with code along, gonna do assignments from this weeks class

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-
+import { useSelector, useDispatch } from 'react-redux'
 import './App.css'
+import { increment, decrement, incrementByAmount } from './reducers/counterSlice'
 
 function App() {
- 
+  const {count} = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   return (
-    <>
-    
-    </>
+    <div className="App">
+      <h1>The count is: {count}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(incrementByAmount(33))}>+ 33</button>
+    </div>
   )
 }
 
